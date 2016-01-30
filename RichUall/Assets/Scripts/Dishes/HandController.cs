0eb94f7f60943bd39d9a693c444fb7e35b2327f9
Charlie Hastings <chastings123@gmail.com>
2016-01-30 13:26:13 -0600

    Add hand movement
    CHECK IN ALL THE THINGS

--- RichUall/Assets/Scripts/Dishes/HandController.cs
+++ RichUall/Assets/Scripts/Dishes/HandController.cs
@@ -2,14 +2,24 @@
 using System.Collections;
 
 public class HandController : MonoBehaviour {
-
+	public float HandSpeed;
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+
+		var x = GetComponent<Rigidbody2D> ().position.x; 
+
+		if (Input.GetAxis ("Horizontal") < 0) {
+			x -= HandSpeed;
+		}
+		if (Input.GetAxis ("Horizontal") > 0) {
+			x += HandSpeed;
+		}
+
+		GetComponent<Rigidbody2D>().position = new Vector2(x, GetComponent<Rigidbody2D>().position.y);
 	}
 }
