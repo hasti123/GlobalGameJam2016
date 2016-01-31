8b4e8dc32c20b27a214a7c18dd27e76189184eb9
Walter <sssplayer6@lycos.com>
2016-01-31 00:43:33 -0600

    Added make bed game start

---
+++ RichUall/Assets/Scripts/MakeBed/MakeBedController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MakeBedController : MonoBehaviour
+{
+    public int winningFluffs;
+    private int _fluffs = 0;
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.E))
+	    {
+	        _fluffs++;
+	    }
+
+	    if (_fluffs >= winningFluffs)
+	    {
+	        SceneManager.LoadScene("Bedroom");
+	    }
+	}
+}
